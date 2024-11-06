4a398d74d07a69aae5c15c2465cdf2e221c07a9b
b01l3d3gg137 <mzr.0cx@gmail.com>
2024-11-06 21:53:22 +0700

    feat: disable view-code

--- public/dashboard/jsmmek/crot.js
+++ public/dashboard/jsmmek/crot.js
@@ -177,3 +177,26 @@ function logout() {
 const yearElement = document.getElementById('current-year');
 // Mengupdate konten dengan tahun saat ini
  yearElement.textContent = new Date().getFullYear();
+
+        document.addEventListener("keydown", function(event) {
+            // Menonaktifkan Ctrl + U (View Source)
+            if (event.ctrlKey && event.keyCode === 85) { 
+                event.preventDefault(); 
+            }
+            
+            // Menonaktifkan F12 (Developer Tools)
+            if (event.keyCode === 123) { 
+                event.preventDefault(); 
+            }
+            
+            // Menonaktifkan Ctrl + Shift + I (Inspect Element)
+            if (event.ctrlKey && event.shiftKey && event.keyCode === 73) { 
+                event.preventDefault(); 
+            }
+        });
+
+                // Mencegah klik kanan (context menu)
+                document.addEventListener("contextmenu", function(event) {
+            event.preventDefault(); // Mencegah menu klik kanan
+        });
+
